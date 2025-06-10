58620d959cb083f12f4be433f6acb9eee1d63649
ALBesana <albesana2015@gmail.com>
2025-06-10 12:31:37 +0800

    working main menu draft

--- Coding/scripts/main.js
+++ Coding/scripts/main.js
@@ -2,6 +2,12 @@ const config = {
     type: Phaser.WEBGL,
     width: 800,
     height: 600,
+    scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH, // Center both horizontally and vertically
+    // Optional:
+    // pageAlignHorizontally: true,
+    // pageAlignVertically: true,
+  },
     physics: {
         default: 'arcade',
         arcade: {
