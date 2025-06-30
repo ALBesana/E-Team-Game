0908fd94495c5b5d9aebb3538dd5acb9600584bb
ALBesana <albesana2015@gmail.com>
2025-06-30 22:08:42 +0800

    fix main menu

--- Coding/scripts/scenes/menu scenes/mainMenu.js
+++ Coding/scripts/scenes/menu scenes/mainMenu.js
@@ -4,38 +4,38 @@ class mainMenu extends Phaser.Scene {
     }
 
     create() {
-        this.menuBackground = this.add.tileSprite(0, 0, 3508, 2480, 'menuBg1').setScale(1).setOrigin(0, 0);
-        this.menuTitle = this.add.image(this.scale.width / 2.85, 133, 'gameTitle').setScale(1);
+        this.menuBackground = this.add.tileSprite(0, 0, 1052, 744, 'menuBg1').setScale(0.92).setOrigin(0, 0);
+        this.menuTitle = this.add.image(this.scale.width / 3.15, 125, 'gameTitle').setScale(0.72);
 
-        this.menuStart = this.add.image(this.scale.width / 4.3, 300, 'startBtn').setScale(0.07).setInteractive();
+        this.menuStart = this.add.image(this.scale.width / 5, 280, 'startBtn').setScale(0.093).setInteractive();
         this.menuStart.on('pointerdown', () => {
             this.time.delayedCall(150, () => {
                 this.scene.start('levelOne');
             });
         });
 
-        this.menuStats = this.add.image(210, 420, 'statsBtn').setScale(0.07).setInteractive();
+        this.menuStats = this.add.image(190, 405, 'statsBtn').setScale(0.093).setInteractive();
         this.menuStats.on('pointerdown', () => {
             this.time.delayedCall(150, () => {
                 this.scene.start('statsScene');
             });
         });
 
-        this.menuCreds = this.add.image(230, 550, 'creditBtn').setScale(0.4).setInteractive();
+        this.menuCreds = this.add.image(230, 534, 'creditBtn').setScale(0.38).setInteractive();
         this.menuCreds.on('pointerdown', () => {
             this.time.delayedCall(150, () => {
                 this.scene.start('creditsScene');
             });
         });
 
-        this.menuSettings = this.add.image(835, 600, 'settingsBtn').setScale(0.12).setInteractive();
+        this.menuSettings = this.add.image(845, 600, 'settingsBtn').setScale(0.09).setInteractive();
         this.menuSettings.on('pointerdown', () => {
             this.time.delayedCall(150, () => {
                 this.scene.start('settingsScene');
             });
         });
 
-        this.menuExit = this.add.image(900, 600, 'exitBtn').setScale(0.13).setInteractive();
+        this.menuExit = this.add.image(900, 600, 'exitBtn').setScale(0.11).setInteractive();
         this.menuExit.on('pointerdown', () => {
             alert('You have quit the game.');
             this.time.delayedCall(150, () => {
